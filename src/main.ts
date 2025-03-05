4e55477f2da1738e4fa24e5494e1f4ba1fab217b
Edgar Vergara <edgarvm13@hotmail.com>
2025-03-05 12:43:44 +0100

    add validation and DTO for payment session; integrate class-transformer and class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config/envs';
 
 async function bootstrap() {
   const logger = new Logger('Payments Microservice');
 
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(envs.port);
 
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,29 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { envs } from 'src/config/envs';
 import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.dto';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeKey);
 
-  async createPaymentSession() {
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items } = paymentSessionDto;
+
+    const lineItems = items.map((item) => ({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: Math.round(item.price * 100),
+      },
+      quantity: item.quantity,
+    }));
+
     const session = await this.stripe.checkout.sessions.create({
       // place order id
       payment_intent_data: {
         metadata: {},
       },
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'T-Shirt',
-            },
-            unit_amount: 2000, // 20 usd
-          },
-          quantity: 2,
-        },
-      ],
+      line_items: lineItems,
       mode: 'payment',
       success_url: 'http://localhost:3003/payments/success',
       cancel_url: 'http://localhost:3003/payments/cancel',
